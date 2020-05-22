1d312becde60a2a21d8550ce342f225187bf7330
Daniel Richtmann <daniel.richtmann@gmail.com>
2020-05-22 15:09:49 +0200

    Adapt blockMaskedInnerProduct to version I put into Grid

--- src/Benchmark_blockMaskedInnerProduct.cc
+++ src/Benchmark_blockMaskedInnerProduct.cc
@@ -4,7 +4,7 @@
 
     Source file: ./benchmarks/Benchmark_blockMaskedInnerProduct.cc
 
-    Copyright (C) 2015 - 2018
+    Copyright (C) 2015 - 2020
 
 Author: Daniel Richtmann <daniel.richtmann@ur.de>
 
@@ -45,6 +45,19 @@ using namespace Grid::BenchmarkHelpers;
 #define NBASIS 40
 #endif
 
+template<class CComplex, int nbasis>
+void convertLayout(std::vector<Lattice<CComplex>> const& in, Lattice<iVector<CComplex, nbasis>>& out) {
+  assert(in.size() == nbasis);
+  for(auto const& elem : in) conformable(elem, out);
+
+  auto  out_v = out.View();
+  auto  in_vc = getViewContainer(in);
+  auto* in_va = &in_vc[0];
+  accelerator_for(ss, out.Grid()->oSites(), CComplex::Nsimd(), {
+    for(int i = 0; i < nbasis; i++) { coalescedWrite(out_v[ss](i), in_va[i](ss)); }
+  });
+}
+
 int main(int argc, char** argv) {
   Grid_init(&argc, &argv);
 
@@ -57,7 +70,7 @@ int main(int argc, char** argv) {
   const int  nB        = nBasis / 2;
   Coordinate blockSize = readFromCommandLineCoordinate(&argc, &argv, "--blocksize", Coordinate({4, 4, 4, 4}));
   int        gsPasses  = readFromCommandLineInt(&argc, &argv, "--gspasses", 1);
-  int        nrhs      = readFromCommandLineInt(&argc, &argv, "--nrhs", 20);
+  int        Ls        = readFromCommandLineInt(&argc, &argv, "--Ls", 1);
   uint64_t   nIterMin  = readFromCommandLineInt(&argc, &argv, "--miniter", 1000);
   uint64_t   nSecMin   = readFromCommandLineInt(&argc, &argv, "--minsec", 5);
   // clang-format on
@@ -70,25 +83,26 @@ int main(int argc, char** argv) {
 
   Coordinate clatt = calcCoarseLattSize(GridDefaultLatt(), blockSize);
 
-#if 1 // 5d use case (u = gauge, f = fermion = fine, t = tmp, c = coarse)
   GridCartesian*         UGrid_f   = SpaceTimeGrid::makeFourDimGrid(GridDefaultLatt(), GridDefaultSimd(Nd, vComplex::Nsimd()), GridDefaultMpi());
   GridRedBlackCartesian* UrbGrid_f = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid_f);
-  GridCartesian*         FGrid_f   = SpaceTimeGrid::makeFiveDimGrid(nrhs, UGrid_f);
-  GridRedBlackCartesian* FrbGrid_f = SpaceTimeGrid::makeFiveDimRedBlackGrid(nrhs, UGrid_f);
   GridCartesian*         UGrid_c   = SpaceTimeGrid::makeFourDimGrid(clatt, GridDefaultSimd(Nd, vComplex::Nsimd()), GridDefaultMpi());
   GridRedBlackCartesian* UrbGrid_c = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid_c);
-  GridCartesian*         FGrid_c   = SpaceTimeGrid::makeFiveDimGrid(nrhs, UGrid_c);
-  GridRedBlackCartesian* FrbGrid_c = SpaceTimeGrid::makeFiveDimRedBlackGrid(nrhs, UGrid_c);
-#else // 4d use case (f = fine, c = coarse)
-  GridCartesian*         UGrid_f   = SpaceTimeGrid::makeFourDimGrid(GridDefaultLatt(), GridDefaultSimd(Nd, vComplex::Nsimd()), GridDefaultMpi());
-  GridRedBlackCartesian* UrbGrid_f = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid_f);
-  GridCartesian*         FGrid_f   = UGrid_f;
-  GridRedBlackCartesian* FrbGrid_f = UrbGrid_f;
-  GridCartesian*         UGrid_c   = SpaceTimeGrid::makeFourDimGrid(clatt, GridDefaultSimd(Nd, vComplex::Nsimd()), GridDefaultMpi());
-  GridRedBlackCartesian* UrbGrid_c = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid_c);
-  GridCartesian*         FGrid_c   = UGrid_c;
-  GridRedBlackCartesian* FrbGrid_c = UrbGrid_c;
-#endif
+  GridCartesian*         FGrid_f   = nullptr;
+  GridRedBlackCartesian* FrbGrid_f = nullptr;
+  GridCartesian*         FGrid_c   = nullptr;
+  GridRedBlackCartesian* FrbGrid_c = nullptr;
+
+  if(Ls != 1) {
+    FGrid_f   = SpaceTimeGrid::makeFiveDimGrid(Ls, UGrid_f);
+    FrbGrid_f = SpaceTimeGrid::makeFiveDimRedBlackGrid(Ls, UGrid_f);
+    FGrid_c   = SpaceTimeGrid::makeFiveDimGrid(Ls, UGrid_c);
+    FrbGrid_c = SpaceTimeGrid::makeFiveDimRedBlackGrid(Ls, UGrid_c);
+  } else {
+    FGrid_f   = UGrid_f;
+    FrbGrid_f = UrbGrid_f;
+    FGrid_c   = UGrid_c;
+    FrbGrid_c = UrbGrid_c;
+  }
 
   UGrid_f->show_decomposition();
   FGrid_f->show_decomposition();
@@ -149,10 +163,10 @@ int main(int argc, char** argv) {
   const int checkOrthog = 1;
   const int projectSpeed = 1;
 
-  UpstreamAggregation UpstreamAggs(UGrid_c, UGrid_f, cb);
-  TwoSpinAggregation TwoSpinAggsFast(UGrid_c, UGrid_f, cb, projectSpeed);
+  UpstreamAggregation UpstreamAggs(FGrid_c, FGrid_f, cb);
+  TwoSpinAggregation TwoSpinAggsFast(FGrid_c, FGrid_f, cb, projectSpeed);
 
-  UpstreamAggs.CreateSubspaceRandom(UPRNG_f);
+  UpstreamAggs.CreateSubspaceRandom(FPRNG_f);
 
   for(int i = 0; i < TwoSpinAggsFast.Subspace().size(); ++i)
     TwoSpinAggsFast.Subspace()[i] = UpstreamAggs.subspace[i];
@@ -165,14 +179,16 @@ int main(int argc, char** argv) {
   //             Calculate numbers needed for performance figures            //
   /////////////////////////////////////////////////////////////////////////////
 
-  auto USiteElems_f = getSiteElems<LatticeFermion>();
-  auto USiteElems_c = getSiteElems<UpstreamCoarseVector>();
+  auto siteElems_f = getSiteElems<LatticeFermion>();
+  auto siteElems_c = getSiteElems<UpstreamCoarseVector>();
 
-  std::cout << GridLogDebug << "USiteElems_f = " << USiteElems_f << std::endl;
-  std::cout << GridLogDebug << "USiteElems_c = " << USiteElems_c << std::endl;
+  std::cout << GridLogDebug << "siteElems_f = " << siteElems_f << std::endl;
+  std::cout << GridLogDebug << "siteElems_c = " << siteElems_c << std::endl;
 
   double UVolume_f = std::accumulate(UGrid_f->_fdimensions.begin(), UGrid_f->_fdimensions.end(), 1, std::multiplies<double>());
+  double FVolume_f = std::accumulate(FGrid_f->_fdimensions.begin(), FGrid_f->_fdimensions.end(), 1, std::multiplies<double>());
   double UVolume_c = std::accumulate(UGrid_c->_fdimensions.begin(), UGrid_c->_fdimensions.end(), 1, std::multiplies<double>());
+  double FVolume_c = std::accumulate(FGrid_c->_fdimensions.begin(), FGrid_c->_fdimensions.end(), 1, std::multiplies<double>());
 
   /////////////////////////////////////////////////////////////////////////////
   //                 Set up stuff required for the benchmark                 //
@@ -188,20 +204,20 @@ int main(int argc, char** argv) {
   typedef TwoSpinCoarsenedMatrix::FermionField TwoSpinCoarseFermionField;
   typedef TwoSpinCoarsenedMatrix::FineScalarField TwoSpinFineScalarField;
 
-  Grid::Upstream::Geometry geom(UGrid_c->_ndimension);
+  Grid::Upstream::Geometry geom(FGrid_c->_ndimension);
 
-  std::vector<UpstreamFineComplexField> masks(geom.npoint, UGrid_f);
+  std::vector<UpstreamFineComplexField> masks(geom.npoint, FGrid_f);
   std::vector<CoarseningLookupTable> lut(geom.npoint);
 
   {
-    UpstreamFineComplexField one(UGrid_f); one = UpstreamScalarType(1.0, 0.0);
-    UpstreamFineComplexField zero(UGrid_f); zero = UpstreamScalarType(0.0, 0.0);
-    UpstreamCoor             coor(UGrid_f);
+    UpstreamFineComplexField one(FGrid_f); one = UpstreamScalarType(1.0, 0.0);
+    UpstreamFineComplexField zero(FGrid_f); zero = UpstreamScalarType(0.0, 0.0);
+    UpstreamCoor             coor(FGrid_f);
 
     for(int p = 0; p < geom.npoint; ++p) {
       int     dir   = geom.directions[p];
       int     disp  = geom.displacements[p];
-      Integer block = (UGrid_f->_rdimensions[dir] / UGrid_c->_rdimensions[dir]);
+      Integer block = (FGrid_f->_rdimensions[dir] / FGrid_c->_rdimensions[dir]);
 
       LatticeCoordinate(coor, dir);
 
@@ -213,7 +229,7 @@ int main(int argc, char** argv) {
         masks[p] = where(mod(coor, block) == (Integer)0, one, zero);
       }
 
-      lut[p].populate(UGrid_c, masks[p]);
+      lut[p].populate(FGrid_c, masks[p]);
     }
   }
 
@@ -226,39 +242,50 @@ int main(int argc, char** argv) {
     std::cout << GridLogMessage << "Running benchmark for ProjectToSubspace" << std::endl;
     std::cout << GridLogMessage << "***************************************************************************" << std::endl;
 
-    LatticeFermion vec_src(UGrid_f);
-    std::vector<UpstreamCoarseComplexField> vec_res_upstream(nBasis, UGrid_c);
-    std::vector<TwoSpinCoarseFermionField> vec_res_twospin(TwoSpinAggregation::Ns_c, UGrid_c);
-    std::vector<UpstreamCoarseFermionField> vec_res_improved(1, UGrid_c);
+    LatticeFermion src(FGrid_f);
+    std::vector<UpstreamCoarseComplexField> res_upstream(nBasis, FGrid_c);
+    std::vector<TwoSpinCoarseFermionField> res_twospin(TwoSpinAggregation::Ns_c, FGrid_c);
+    std::vector<UpstreamCoarseFermionField> res_improved(1, FGrid_c);
 
-    random(UPRNG_f, vec_src);
+    random(FPRNG_f, src);
 
     for(int myPoint=0; myPoint<geom.npoint; ++myPoint) {
-      for(auto& elem : vec_res_upstream) elem = Zero();
-      for(auto& elem : vec_res_twospin) elem = Zero();
-      for(auto& elem : vec_res_improved) elem = Zero();
+      for(auto& elem : res_upstream) elem = Zero();
+      for(auto& elem : res_twospin) elem = Zero();
+      for(auto& elem : res_improved) elem = Zero();
 
       auto workSites_f = real(TensorRemove(sum(masks[myPoint]))); // remove sites with zero in its mask from flop and byte counting
+
       std::cout << GridLogMessage << "point = " << myPoint
                 << ": workSites, volume, ratio = "
-                << workSites_f << ", " << UVolume_f << ", " << workSites_f / UVolume_f << std::endl;
+                << workSites_f << ", " << FVolume_f << ", " << workSites_f / FVolume_f << std::endl;
 
-      double flop = workSites_f * (8 * USiteElems_f) * nBasis;
-      double byte = workSites_f * (2 * 1 + 2 * USiteElems_f) * nBasis * sizeof(Complex);
+      double flop = workSites_f * (8 * siteElems_f) * nBasis;
+      double byte = workSites_f * (2 * 1 + 2 * siteElems_f) * nBasis * sizeof(Complex);
 
       BenchmarkFunctionMRHS(Grid::Upstream::blockMaskedInnerProduct,
                             flop, byte, nIterMin, nSecMin, nBasis,
-                            vec_res_upstream[rhs], masks[myPoint], UpstreamAggs.subspace[rhs], vec_src);
+                            res_upstream[rhs], masks[myPoint], UpstreamAggs.subspace[rhs], src);
 
       BenchmarkFunctionMRHS(Grid::UpstreamImprovedDirsaveLut::blockLutedInnerProduct,
                             flop, byte, nIterMin, nSecMin, 1,
-                            vec_res_improved[rhs], vec_src, UpstreamAggs.subspace, lut[myPoint]);
+                            res_improved[rhs], src, UpstreamAggs.subspace, lut[myPoint]);
 
       BenchmarkFunctionMRHS(TwoSpinAggregation::Kernels::aggregateProjectFast,
                             flop, byte, nIterMin, nSecMin, TwoSpinCoarsenedMatrix::Ns_c,
-                            vec_res_twospin[rhs], vec_src, TwoSpinAggsFast.Subspace(), lut[myPoint]);
+                            res_twospin[rhs], src, TwoSpinAggsFast.Subspace(), lut[myPoint]);
 
-      // TODO: Compare results
+      UpstreamCoarseFermionField tmp_improved(FGrid_c);
+      UpstreamCoarseFermionField diff(FGrid_c);
+      convertLayout(res_upstream, tmp_improved);
+      diff = res_improved[0] - tmp_improved;
+      auto diff2 = norm2(diff);
+
+      std::cout << GridLogDebug << "Result upstream(mask) = " << norm2(tmp_improved) << " result improved(lut) = " << norm2(res_improved[0]) << " diff = " << diff2 << std::endl;
+
+      assert(diff2 == 0.);
+
+      std::cout << GridLogMessage << "Results are equal" << std::endl;
     }
   }
 
